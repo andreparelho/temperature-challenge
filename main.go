6e44100b29a0053e7cc3ed40c3aff9d607c061fb
Andre Parelho <parelhoneves.andre@gmail.com>
2024-12-26 21:49:36 -0300

    feat: create main

--- main.go
+++ main.go
@@ -1,5 +1,42 @@
 package main
 
-func main() {
+import (
+	"math/rand/v2"
 
+	"github.com/andreparelho/temperature-challenge/channel"
+	"github.com/andreparelho/temperature-challenge/observer"
+	"github.com/andreparelho/temperature-challenge/zookeeper"
+)
+
+const MAX_ITERATOR int = 500
+
+func main() {
+	var numbers []int
+	var newChannel = make(chan int)
+
+	var channel = channel.TemperatureChannel{
+		Channel: newChannel,
+	}
+
+	var observer = observer.NewObserver{
+		TemperatureChannel: channel,
+	}
+
+	var zookeeperOne = zookeeper.NewZookeeperOne{
+		Numbers:            numbers,
+		TemperatureChannel: channel,
+	}
+
+	var zookeeperTwo = zookeeper.NewZookeeperTwo{
+		Numbers:            numbers,
+		TemperatureChannel: channel,
+	}
+
+	for i := 0; i < MAX_ITERATOR; i++ {
+		var randomNumber = rand.IntN(100)
+
+		go observer.Observer(randomNumber)
+		go zookeeper.IZookeeper.Consumer(&zookeeperOne)
+		go zookeeper.IZookeeper.Consumer(&zookeeperTwo)
+	}
 }
